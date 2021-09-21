07d33dec523e33b32057780ae4402aca8b3c7c09
Yash Poojary <yashspoojary245@gmail.com>
2021-09-21 14:54:00 +0530

    Initial Commit

---
+++ Word Scrabble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Word Scrabble
+//
+//  Created by Yash Poojary on 21/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Word Scrabble/Word_ScrabbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrabbleApp.swift
+//  Word Scrabble
+//
+//  Created by Yash Poojary on 21/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrabbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
